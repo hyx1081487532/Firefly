5b75a680951010c19f68653e4af6063f2b91baf3
hyx1081487532 <hyx1081487532@outlook.com>
2025-12-02 23:50:37 +0800

    初始提交: Firefly项目完整版本

---
+++ src/config/profileConfig.ts
@@ -0,0 +1,19 @@
+import type { ProfileConfig } from "../types/config";
+
+export const profileConfig: ProfileConfig = {
+  avatar: "/assets/images/avatar.webp",
+  name: "xiaoqueqiao",
+  bio: "Hello, I'm xiaoqueqiao.",
+  links: [
+    {
+      name: "Bilibli",
+      icon: "fa6-brands:bilibili",
+      url: "https://space.bilibili.com/3546772846676004",
+    },
+    {
+      name: "GitHub",
+      icon: "fa6-brands:github",
+      url: "https://github.com/hyx1081487532",
+    },
+  ],
+};
