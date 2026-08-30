12e62e100874050eecbd1966b01351e05a891cc6
hyx1081487532 <hyx1081487532@outlook.com>
2026-08-30 19:35:31 +0800

    评论系统

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -1,7 +1,7 @@
 import type { CommentConfig } from "../types/config";
 
 export const commentConfig: CommentConfig = {
-  type: "none", // 当前启用的评论系统类型: none, twikoo, waline, giscus, disqus，默认为none，即不启用评论系统。
+  type: "giscus", // 当前启用的评论系统类型: none, twikoo, waline, giscus, disqus，默认为none，即不启用评论系统。
   //twikoo评论系统配置
   twikoo: {
     envId: "https://twikoo.vercel.app",
@@ -20,8 +20,8 @@ export const commentConfig: CommentConfig = {
   },
   //giscus评论系统配置（还未测试）
   giscus: {
-    repo: "CuteLeaf/Firefly", // 设置 Giscus 评论系统仓库
-    repoId: "R_kgD2gfdFGd", // 设置 Giscus 评论系统仓库ID
+    repo: "hyx1081487532/Firefly", // 设置 Giscus 评论系统仓库
+    repoId: "R_kgDOQhMulg", // 设置 Giscus 评论系统仓库ID
     category: "General", // 设置 Giscus 评论系统分类
     categoryId: "DIC_kwDOKy9HOc4CegmW", // 设置 Giscus 评论系统分类ID
     mapping: "title", // 设置 Giscus 评论系统映射方式
@@ -29,7 +29,7 @@ export const commentConfig: CommentConfig = {
     reactionsEnabled: "1", // 设置 Giscus 评论系统反应功能
     emitMetadata: "1", // 设置 Giscus 评论系统元数据
     inputPosition: "top", // 设置 Giscus 评论系统输入位置
-    theme: "light", // 设置 Giscus 评论系统主题
+    theme: "preferred_color_scheme", // 设置 Giscus 评论系统主题
     lang: "zh-CN", // 设置 Giscus 评论系统语言
     loading: "lazy", // 设置 Giscus 评论系统加载方式
   },
